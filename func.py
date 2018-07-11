4348db8f4bdb83f8a8701afdd485273b25b5a32b
wuli133144 <1683358846@qq.com>
2018-07-11 11:28:13 +0800

    function test

---
+++ func.py
@@ -0,0 +1,29 @@
+
+
+'''
+ simple function use
+'''
+'''
+def  test(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+
+
+
+test(1,3,4)
+
+'''
+
+'''
+mutil argv
+'''
+
+def func(*num):
+    for i in num:
+        print(i)
+
+func(1,2,3,4)
+
+
+
